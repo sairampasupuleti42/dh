6cd7dffbbc2d1588e0e7efdbf104ca68b34b4670
sairampasupuleti42 <sairampasupuleti.42@gmail.com>
2021-10-02 10:29:24 +0530

    2 files modified

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import Slider from 'react-slick';
 export default function Banner() {
-  return <Container>Hello</Container>;
+  return (
+    <Container>
+      <Slider {...settings}>
+        <div>
+          <img src="http://placekitten.com/g/400/200" />
+        </div>
+        <div>
+          <img src="http://placekitten.com/g/400/200" />
+        </div>
+        <div>
+          <img src="http://placekitten.com/g/400/200" />
+        </div>
+        <div>
+          <img src="http://placekitten.com/g/400/200" />
+        </div>
+      </Slider>
+    </Container>
+  );
 }
 const Container = styled.main`
   
